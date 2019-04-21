40ce611494dc7d8ed696936f6cf6c678dc343ccc
beccasiegal <46900478+beccasiegal@users.noreply.github.com>
2019-04-21 12:32:16 -0400

    Create datastore.js

---
+++ datastore.js
@@ -0,0 +1,113 @@
+const STORE = [
+  {
+    question: 'Which position player is known as "1" on the baseball diamond?',
+    answers: [
+      'Pitcher',
+      'Catcher',
+      'Shortstop',
+      'First Baseman',
+    ],
+    correctAnswer: 'Pitcher'
+  },
+
+
+  {
+    question: 'How many players play defense on a baseball diamond?',
+    answers:[
+      '10',
+      '12',
+      '9',
+      '8',
+    ],
+    correctAnswer: '9'
+ },
+
+
+  {
+    question: 'The DH hits instead of the __.',
+    answers: [
+      'Shortstop',
+      'Pitcher',
+      'Catcher',
+      'Center Fielder',
+   ],
+    correctAnswer: 'Pitcher'
+  },
+
+{
+  question: 'What is an error?',
+  answers: [
+    'Striking out',
+    'Running out of the basepaths',
+    'Making a mistake on defense',
+    'Getting a single',
+  ],
+  correctAnswer: 'Making a mistake on defense'
+},
+
+{
+  question: 'What is a full count?',
+  answers: ['3-0',
+    '3-2',
+    '2-2',
+    '4-0',
+  ],
+  correctAnswer: '3-2'
+},
+
+{
+  question: 'How many strikes does it take to make an out?',
+  answers: [
+    '0',
+    '1',
+    '2',
+    '3',
+  ],
+  correctAnswer: '3'
+},
+
+{
+  question: 'What do players use to hit the ball?',
+  answers: [
+    'glove',
+    'bat',
+    'hands',
+    'head',
+  ],
+  correctAnswer: 'bat'
+},
+
+{
+  question: 'What are the different levels of major league baseball\'s farm system?',
+  answers: [
+  	'A, AA, AAA',
+    'X, Y, Z',
+    'A, B, C',
+    'MLB, NHL, MBA, NFL',
+  ],
+  correctAnswer: 'A, AA, AAA'
+},
+
+{
+  question: 'How long is a regular baseball game?',
+  answers: [
+    '10 innings',
+    '9 innings',
+    '15 innings',
+    'until people get tired',
+  ],
+  correctAnswer: '9 innings'
+},
+
+{
+  question: 'How many teams are in the major leagues?',
+  answers: [
+    '15',
+    '20',
+    '100',
+    '30',
+  ],
+  correctAnswer: '30'
+}
+
+]
